bf3e003a258935b9744fc082c9937cd54b804156
Uriel Malka <105796126+urielmalka@users.noreply.github.com>
2023-07-20 17:27:03 +0300

    Update ScraperConfig.py
    Fix payload

--- court_scraper/ScraperConfig.py
+++ court_scraper/ScraperConfig.py
@@ -17,175 +17,95 @@ class ScraperConfig:
         "Sec-Fetch-Site": "same-origin"
     }
 
-
-    payload1 = {
-        "document": {
-            "Year": None,
-            "Counsel": [{
-                "Text": "",
-                "textOperator": 2,
-                "option": "2",
-                "Inverted": False,
-                "Synonym": False,
-                "NearDistance": 3,
-                "MatchOrder": False
-            }],
-            "CaseNum": None,
-            "Technical": None,
-            "fromPages": None,
-            "toPages": None,
-            "dateType": 1,
-            "PublishFrom": "2023-05-28T21:14:20.030Z",
-            "PublishTo": "2023-06-28T21:14:19.808Z",
-            "publishDate": 8,
-            "translationDateType": 1,
-            "translationPublishFrom": "2023-05-28T21:14:19.808Z",
-            "translationPublishTo": "2023-06-28T21:14:19.808Z",
-            "translationPublishDate": 8,
-            "SearchText": [{
-                "Text": "",
-                "textOperator": 1,
-                "option": "2",
-                "Inverted": False,
-                "Synonym": False,
-                "NearDistance": 3,
-                "MatchOrder": False
-            }],
-            "Judges": None,
-            "Parties": [{
-                "Text": "",
-                "textOperator": 2,
-                "option": "2",
-                "Inverted": False,
-                "Synonym": False,
-                "NearDistance": 3,
-                "MatchOrder": False
-            }],
-            "Mador": None,
-            "CodeMador": [],
-            "TypeCourts": None,
-            "TypeCourts1": None,
-            "TerrestrialCourts": None,
-            "LastInyan": None,
-            "LastCourtsYear": None,
-            "LastCourtsMonth": None,
-            "LastCourtCaseNum": None,
-            "Old": False,
-            "JudgesOperator": 2,
-            "Judgment": None,
-            "Type": [{
+    payload = {
+                  "document": {
+                    "Year": None,
+                    "Counsel": [
+                      {
+                        "Text": "",
+                        "textOperator": 2,
+                        "option": "2",
+                        "Inverted": False,
+                        "Synonym": False,
+                        "NearDistance": 3,
+                        "MatchOrder": False
+                      }
+                    ],
+                    "CaseNum": None,
+                    "Technical": None,
+                    "fromPages": None,
+                    "toPages": None,
+                    "dateType": 2,
+                    "PublishFrom": None,
+                    "PublishTo": None,
+                    "publishDate": 8,
+                    "translationDateType": 1,
+                    "translationPublishFrom": "2023-06-20T14:05:05.444Z",
+                    "translationPublishTo": "2023-07-20T14:05:05.444Z",
+                    "translationPublishDate": 8,
+                    "SearchText": [
+                      {
+                        "Text": "",
+                        "textOperator": 1,
+                        "option": "2",
+                        "Inverted": False,
+                        "Synonym": False,
+                        "NearDistance": 3,
+                        "MatchOrder": False
+                      }
+                    ],
+                    "Judges": None,
+                    "Parties": [
+                      {
+                        "Text": "",
+                        "textOperator": 2,
+                        "option": "2",
+                        "Inverted": False,
+                        "Synonym": False,
+                        "NearDistance": 3,
+                        "MatchOrder": False
+                      }
+                    ],
+                    "Mador": None,
+                    "CodeMador": [],
+                    "TypeCourts": None,
+                    "TypeCourts1": None,
+                    "TerrestrialCourts": None,
+                    "LastInyan": None,
+                    "LastCourtsYear": None,
+                    "LastCourtsMonth": None,
+                    "LastCourtCaseNum": None,
+                    "Old": False,
+                    "JudgesOperator": 2,
+                    "Judgment": None,
+                    "Type": [
+                      {
                         "parent": 2,
                         "value": 2,
                         "text": "פסק-דין"
-                      }],
-            "CodeTypes": [],
-            "CodeJudges": [],
-            "Inyan": None,
-            "CodeInyan": [],
-            "AllSubjects": [{
-                "Subject": None,
-                "SubSubject": None,
-                "SubSubSubject": None
-            }],
-            "CodeSub2": [],
-            "Category1": None,
-            "Category3": None,
-            "CodeCategory3": [],
-            "Volume": None,
-            "Subjects": None,
-            "SubSubjects": None,
-            "SubSubSubjects": None
-        },
-        "lan": "1"
-    }
-
-    payload = {
-        "document": {
-            "Year": 2023,
-            "Counsel": [
-                {
-                    "Text": "",
-                    "textOperator": 2,
-                    "option": "2",
-                    "Inverted": False,
-                    "Synonym": False,
-                    "NearDistance": 3,
-                    "MatchOrder": False
+                      }
+                    ],
+                    "CodeTypes": [
+                      2
+                    ],
+                    "CodeJudges": [],
+                    "Inyan": None,
+                    "CodeInyan": [],
+                    "AllSubjects": [
+                      {
+                        "Subject": None,
+                        "SubSubject": None,
+                        "SubSubSubject": None
+                      }
+                    ],
+                    "CodeSub2": [],
+                    "Category1": None,
+                    "Category3": None,
+                    "CodeCategory3": [],
+                    "Volume": None,
+                    "Subjects": None,
+                    "SubSubjects": None,
+                    "SubSubSubjects": None
+                  },
+                  "lan": "1"
                 }
-            ],
-            "CaseNum": None,
-            "Technical": None,
-            "fromPages": None,
-            "toPages": None,
-            "dateType": 1,
-            "PublishFrom": "2023-05-29T11:19:55.415Z",
-            "PublishTo": "2023-06-29T11:19:55.216Z",
-            "publishDate": 8,
-            "translationDateType": 1,
-            "translationPublishFrom": "2023-05-29T11:19:55.216Z",
-            "translationPublishTo": "2023-06-29T11:19:55.216Z",
-            "translationPublishDate": 8,
-            "SearchText": [
-                {
-                    "Text": "",
-                    "textOperator": 1,
-                    "option": "2",
-                    "Inverted": False,
-                    "Synonym": False,
-                    "NearDistance": 3,
-                    "MatchOrder": False
-                }
-            ],
-            "Judges": None,
-            "Parties": [
-                {
-                    "Text": "",
-                    "textOperator": 2,
-                    "option": "2",
-                    "Inverted": False,
-                    "Synonym": False,
-                    "NearDistance": 3,
-                    "MatchOrder": False
-                }
-            ],
-            "Mador": None,
-            "CodeMador": [],
-            "TypeCourts": None,
-            "TypeCourts1": None,
-            "TerrestrialCourts": None,
-            "LastInyan": None,
-            "LastCourtsYear": None,
-            "LastCourtsMonth": None,
-            "LastCourtCaseNum": None,
-            "Old": False,
-            "JudgesOperator": 2,
-            "Judgment": None,
-            "Type": [
-                {
-                    "parent": 2,
-                    "value": 2,
-                    "text": "פסק-דין"
-                }
-            ],
-            "CodeTypes": [2],
-            "CodeJudges": [],
-            "Inyan": None,
-            "CodeInyan": [],
-            "AllSubjects": [
-                {
-                    "Subject": None,
-                    "SubSubject": None,
-                    "SubSubSubject": None
-                }
-            ],
-            "CodeSub2": [],
-            "Category1": None,
-            "Category3": None,
-            "CodeCategory3": [],
-            "Volume": None,
-            "Subjects": None,
-            "SubSubjects": None,
-            "SubSubSubjects": None
-        },
-        "lan": "1"
-    }
